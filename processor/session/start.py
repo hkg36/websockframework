3033f3e28ef3bb069f52b7e70d054f2454387024
amen <amen@localhost.localdomain>
2014-01-22 16:32:03 +0800

    Signed-off-by: amen <amen@localhost.localdomain>

--- processor/session/start.py
+++ processor/session/start.py
@@ -13,9 +13,9 @@ def run(sessionid):
     with dbconfig.Session() as session:
         user_data=session.query(User).filter(User.uid==data['uid']).first()
         if user_data:
-            session.query(ConnectionInfo).filter(ConnectionInfo.connection_id==BackEndEnvData.connection_id).delete()
-            session.commit()
-            cinfo=ConnectionInfo()
+            cinfo=session.query(ConnectionInfo).filter(ConnectionInfo.connection_id==BackEndEnvData.connection_id).first()
+            if cinfo is None:
+                cinfo=ConnectionInfo()
             cinfo.uid=user_data.uid
             cinfo.connection_id=BackEndEnvData.connection_id
             cinfo.queue_id=BackEndEnvData.reply_queue
