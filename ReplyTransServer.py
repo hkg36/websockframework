1f2f8529c905aa712f37a47273d2fed513ae1153
amen <zju3020912063@gmail.com>
2014-01-23 15:20:04 +0800

    update

--- ReplyTransServer.py
+++ ReplyTransServer.py
@@ -1,4 +1,5 @@
 #coding:utf-8
+from sqlalchemy import and_
 import QueueWork
 import getopt
 import importlib
@@ -8,6 +9,8 @@ from datamodel.group import GroupWatchUpdate
 from datamodel.friendlist import FriendList
 from datamodel.group_member import GroupMember
 from datamodel.post import Post
+from datamodel.post_like import PostLike
+from datamodel.post_reply import PostReply
 import dbconfig
 import json
 import zlib
@@ -16,19 +19,30 @@ def RequestWork(params,body,reply_queue):
     reply=json.loads(body)
     postid=reply['postid']
     with dbconfig.Session() as session:
+        to_uid=set()
+        likes=session.query(PostLike).filter(PostLike.postid==postid).all()
+        for a in likes:
+            to_uid.add(a.uid)
+        replys=session.query(PostReply).filter(PostReply.postid==postid).order_by(PostReply.replyid.desc()).limit(50).all()
+        for a in replys:
+            to_uid.add(a.uid)
         post=session.query(Post).filter(Post.postid==postid).first()
-        if post.uid==reply['uid']:
-            return
-        conn=session.query(ConnectionInfo).filter(ConnectionInfo.uid==post.uid).first()
+        to_uid.add(post.uid)
+        if reply['uid'] in to_uid:
+            to_uid.remove(reply['uid'])
+
+        conns=session.query(ConnectionInfo).filter(ConnectionInfo.uid.in_(list(to_uid))).all()
         to_push=json.dumps({"push":True,
                                 "type":"newreply",
                                 "data":{
+                                    "gid":post.group_id,
                                     "reply":reply
                                 }
                             })
-        QueueWork.producer.publish(body=to_push,delivery_mode=2,headers={"connid":conn.connection_id},
-                                  routing_key=conn.queue_id,
-                                  compression='gzip')
+        for conn in conns:
+            QueueWork.producer.publish(body=to_push,delivery_mode=2,headers={"connid":conn.connection_id},
+                                      routing_key=conn.queue_id,
+                                      compression='gzip')
 if __name__ == '__main__':
     config_model='configs.frontend'
     opts, args=getopt.getopt(sys.argv[1:],'c:',
