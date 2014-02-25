9c545c98ea8fca14e301753f550e53e4c352740a
amen <zju3020912063@gmail.com>
2014-02-25 17:23:17 +0800

    Signed-off-by: amen <zju3020912063@gmail.com>

--- processor/group/invite.py
+++ processor/group/invite.py
@@ -1,6 +1,8 @@
 #coding:utf-8
+from sqlalchemy import and_
 from datamodel.events import Events
 from datamodel.group import Group
+from datamodel.group_member import GroupMember
 from tools.addPushQueue import AddEventNotify
 from tools.helper import Res
 from tools.session import CheckSession
@@ -13,9 +15,15 @@ def run(gid,uid):
     if isinstance(uid,list)==False:
         uid=[uid]
     with dbconfig.Session() as session:
+
         group=session.query(Group).filter(Group.gid==gid).first()
         if group is None or (group.everyone_caninvite==0 and group.creator!=BackEndEnvData.uid):
             return Res(errno=2,error="not everyone can invaite and you are not creator")
+        if group.creator!=uid:
+            gm=session.query(GroupMember).filter(and_(GroupMember.gid==gid,GroupMember.uid==uid)).first()
+            if gm is None:
+                return Res(errno=2,error="you are not member")
+
         events=[]
         for u in uid:
             event=Events()
