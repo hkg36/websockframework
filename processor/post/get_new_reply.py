4349426078e08bfc14f23e2866a40faa178dbf92
amen <amen@localhost.localdomain>
2014-03-19 22:33:56 -0400

    Signed-off-by: amen <amen@localhost.localdomain>

--- processor/post/get_new_reply.py
+++ processor/post/get_new_reply.py
@@ -1,7 +1,9 @@
 #coding:utf-8
+import datetime
 from sqlalchemy import and_
 
 from datamodel.post import Post
+from datamodel.post_like import PostLike
 from datamodel.post_reply import PostReply
 from tools.helper import Res
 from tools.session import CheckSession
@@ -12,9 +14,9 @@ import BackEndEnvData
 import dbconfig
 
 @CheckSession(level=0)
-def run(from_reply=0):
+def run(from_reply=0,like_after=None):
     with dbconfig.Session() as session:
-        reply_list=[]
+
         post_ids=set()
 
         posts=session.query(Post).filter(Post.uid).order_by(Post.postid.desc()).limit(20).all()
@@ -24,12 +26,21 @@ def run(from_reply=0):
         for reply in replys:
             post_ids.add(reply.postid)
 
+        result={}
         if len(post_ids)==0:
-            return Res({"replys":[]})
-
-        subquery=session.query(PostReply)
-        subquery=subquery.filter(and_(PostReply.postid.in_(list(post_ids)),PostReply.replyid>from_reply))
-        replys=subquery.order_by(PostReply.replyid).limit(100).all()
-        for reply in replys:
-            reply_list.append(reply.toJson())
-        return Res({"replys":reply_list})
+            result["replys"]=[]
+        else:
+            reply_list=[]
+            subquery=session.query(PostReply)
+            subquery=subquery.filter(and_(PostReply.postid.in_(list(post_ids)),PostReply.replyid>from_reply))
+            replys=subquery.order_by(PostReply.replyid).limit(100).all()
+            for reply in replys:
+                reply_list.append(reply.toJson())
+            result["replys"]=reply_list
+        if like_after is not None:
+            likes=session.query(PostLike).filter(and_(PostLike.postid.in_(list(post_ids)),PostLike.time>datetime.datetime.fromtimestamp(like_after))).order_by(PostLike.time.desc()).limit(50).all()
+            likelist=[]
+            for like in likes:
+                likelist.append(like.toJson())
+            result['likes']=likelist
+        return Res(result)
