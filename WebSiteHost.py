816645bf3c8d7ef7bd710c1ebc71757f2a8885a4
amen <amen@localhost.localdomain>
2014-04-24 08:40:27 -0400

    Signed-off-by: amen <amen@localhost.localdomain>

--- WebSiteHost.py
+++ WebSiteHost.py
@@ -6,5 +6,5 @@ from WebSiteBasePage import LoadPageList
 web.config.debug = False
 path_list=LoadPageList()
 webapp=web.application(path_list, locals())
-
+del path_list
 application = webapp.wsgifunc()
