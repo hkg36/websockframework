3bf28ca1bd9defa9d25e7e416675208dfa529737
amen <amen@localhost.localdomain>
2014-08-08 05:22:35 -0400

    Signed-off-by: amen <amen@localhost.localdomain>

--- datamodel/user.py
+++ datamodel/user.py
@@ -14,7 +14,7 @@ class User(dbconfig.DBBase):
     password=Column(String(128))
     headpic=Column(String(1024))
     sex=Column(Integer,default=0)
-    birthday = Column(DateTime)
+    birthday = Column(Integer)
     marriage = Column(Integer)
     background_image = Column(String(1024))
     height = Column(Integer, default=0)
