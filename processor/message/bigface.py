8e9748b1ec248687558c43cbda79ce2dd0521748
amen <amen@localhost.localdomain>
2014-09-02 00:28:09 -0400

    Signed-off-by: amen <amen@localhost.localdomain>

---
+++ processor/message/bigface.py
@@ -0,0 +1,41 @@
+from tools.helper import Res
+
+__author__ = 'amen'
+def run():
+
+    face=[
+      "http://cdmmstatic.qiniudn.com/sticker_126361874215276@2x.png",
+      "http://cdmmstatic.qiniudn.com/sticker_126361884215275@2x.png",
+      "http://cdmmstatic.qiniudn.com/sticker_126361890881941@2x.png",
+      "http://cdmmstatic.qiniudn.com/sticker_126361900881940@2x.png",
+      "http://cdmmstatic.qiniudn.com/sticker_126361910881939@2x.png",
+      "http://cdmmstatic.qiniudn.com/sticker_126361920881938@2x.png",
+      "http://cdmmstatic.qiniudn.com/sticker_126361957548601@2x.png",
+      "http://cdmmstatic.qiniudn.com/sticker_126361967548600@2x.png",
+      "http://cdmmstatic.qiniudn.com/sticker_126361974215266@2x.png",
+      "http://cdmmstatic.qiniudn.com/sticker_126361980881932@2x.png",
+      "http://cdmmstatic.qiniudn.com/sticker_126361987548598@2x.png",
+      "http://cdmmstatic.qiniudn.com/sticker_126361994215264@2x.png",
+      "http://cdmmstatic.qiniudn.com/sticker_126362007548596@2x.png",
+      "http://cdmmstatic.qiniudn.com/sticker_126362027548594@2x.png",
+      "http://cdmmstatic.qiniudn.com/sticker_126362034215260@2x.png",
+      "http://cdmmstatic.qiniudn.com/sticker_126362044215259@2x.png",
+      "http://cdmmstatic.qiniudn.com/sticker_126362064215257@2x.png",
+      "http://cdmmstatic.qiniudn.com/sticker_126362074215256@2x.png",
+      "http://cdmmstatic.qiniudn.com/sticker_126362080881922@2x.png",
+      "http://cdmmstatic.qiniudn.com/sticker_126362087548588@2x.png",
+      "http://cdmmstatic.qiniudn.com/sticker_126362100881920@2x.png",
+      "http://cdmmstatic.qiniudn.com/sticker_126362107548586@2x.png",
+      "http://cdmmstatic.qiniudn.com/sticker_126362117548585@2x.png",
+      "http://cdmmstatic.qiniudn.com/sticker_126362124215251@2x.png",
+      "http://cdmmstatic.qiniudn.com/sticker_126362130881917@2x.png",
+      "http://cdmmstatic.qiniudn.com/sticker_126362137548583@2x.png",
+      "http://cdmmstatic.qiniudn.com/sticker_126362160881914@2x.png",
+      "http://cdmmstatic.qiniudn.com/sticker_126362167548580@2x.png",
+      "http://cdmmstatic.qiniudn.com/sticker_126362180881912@2x.png",
+      "http://cdmmstatic.qiniudn.com/sticker_126362187548578@2x.png",
+      "http://cdmmstatic.qiniudn.com/sticker_126362197548577@2x.png",
+      "http://cdmmstatic.qiniudn.com/sticker_126362207548576@2x.png",
+      "http://cdmmstatic.qiniudn.com/sticker_126362230881907@2x.png"
+    ]
+    return Res({'face':face})
--- tests/clearquniu.py
+++ tests/clearquniu.py
@@ -1,16 +1,15 @@
 #coding:utf-8
 import datetime
-
+import json
 import qiniu.rsf
 import qiniu.conf
 import qiniu.rs
 
 
-qiniu.conf.ACCESS_KEY = "x5yGWWp6fBGMwlJyEU0GVzilkNIa7Mc87ibrKpdU"
-qiniu.conf.SECRET_KEY = "r_8i1p4LCaiI0isFxuF2paAKhoQotGeqngCD4B1O"
-qiniuSpace="kidswant"
+qiniu.conf.ACCESS_KEY = "W4nYpf8HOCEyCzjLHpO0QVYGOykRucI1MIniLpgL"
+qiniu.conf.SECRET_KEY = "3NRgrf8v_XHGGoWROaXYZoFYPvSeN3HnI_19eVfk"
+qiniuSpace="cdmmstatic"
 
-beforetime=datetime.datetime(year=2014,month=3,day=10)
 todel_file=[]
 
 rs = qiniu.rsf.Client()
@@ -20,17 +19,6 @@ while err is None:
     ret, err = rs.list_prefix(qiniuSpace, prefix="", limit=1000, marker=marker)
     marker = ret.get('marker', None)
     for item in ret['items']:
-        dtime=datetime.datetime.fromtimestamp(item['putTime']/10e6)
-        timedelta=dtime-beforetime
-        if timedelta.total_seconds()<0:
-            todel_file.append(item)
-if err is not qiniu.rsf.EOF:
-    # 错误处理
-    pass
+        todel_file.append("http://cdmmstatic.qiniudn.com/"+item['key'])
 
-delkeys=[]
-for delf in todel_file:
-    path_1 = qiniu.rs.EntryPath(qiniuSpace, delf['key'])
-    delkeys.append(path_1)
-rets, err = qiniu.rs.Client().batch_delete(delkeys)
-print(rets)
+print json.dumps(todel_file,ensure_ascii=False,indent=2)
